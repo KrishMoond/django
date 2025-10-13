d91e350535cbef58ceb7f563e6195a01332e0487
unknown <moondkrish921@gmail.com>
2025-10-13 10:52:09 +0530

    mailer-added

--- mailer/mailer/settings.py
+++ mailer/mailer/settings.py
@@ -128,6 +128,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "moondkrish921@gmail.com"
-EMAIL_HOST_PASSWORD = 'bbewrsjwehnajrpo'
+EMAIL_HOST_PASSWORD = your_app_password_here
 DEFAULT_FROM_EMAIL =EMAIL_HOST_USER
 EMAIL_USE_LOCALTIME = True
